c8127145d48e8da7abb1d580d4bc8a555973451d
Jason <jasonmcnamara628@gmail.com>
2018-11-19 19:26:48 -0500

    come on man

--- src/components/ui/header/header.js
+++ src/components/ui/header/header.js
@@ -3,8 +3,6 @@ import styles from './header.css';
 import navigation from '../navigation/navigation';
 import navbtn from '../navbtn/navbtn';
 
-import coffeePic from '../../../images/CoffeePic.jpg';
-
 const header = (()=>{
     const body = document.querySelector('body');
     const header = document.createElement('div');
@@ -15,7 +13,6 @@ const header = (()=>{
     const headerlogo = document.createElement('div');
     headerlogo.classList.add(styles.headerlogo);
     headerlogo.classList.add(styles.test);
-    headerlogo.style = `backgroundImage: ${coffeePic}`;
     // headerlogo.textContent = 'Logo';
     
     const headerlogolink = document.createElement('a');
